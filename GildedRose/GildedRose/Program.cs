ebcebb2bb6ead1f47818f6d33a0c09689b893c7a
medmondson <eddy556@gmail.com>
2017-11-16 22:31:24 +0000

    Added console front-end

--- GildedRose/GildedRose/Program.cs
+++ GildedRose/GildedRose/Program.cs
@@ -10,6 +10,51 @@ namespace GildedRose
     {
         static void Main(string[] args)
         {
+            InventoryManager im = new InventoryManager();
+
+            Console.WriteLine("Begin adding items to the inventory. Type 'Done' when complete");
+            Console.WriteLine("Example 'Backstage passes -1 2'");
+
+            bool scanning = true;
+
+            while(scanning)
+            {
+                var input = Console.ReadLine();
+
+                if(input != "Done")
+                {
+                    var items = input.Split(' ');
+
+                    if (items.Count() == 3)
+                        im.Add(items[0], int.Parse(items[1]), int.Parse(items[2]));
+
+                    if (items.Count() == 4)
+                        im.Add($"{items[0]} {items[1]}", int.Parse(items[2]), int.Parse(items[3]));
+                }
+                else
+                {
+                    scanning = false;
+
+                    var items = im.IncrementDay();
+
+                    Console.WriteLine();
+                    Console.WriteLine("=== Day has been incremented ===");
+                    
+                    foreach(IDegradeableItem i in items)
+                    {
+                        if(i.Sellin == 0 & i.Quality == 0)
+                        {
+                            Console.WriteLine($"{i.Name}");
+                            continue;
+                        }
+
+                        Console.WriteLine($"{i.Name} {i.Sellin} {i.Quality}");
+                    }
+
+                    Console.Read();
+                }
+            }
+
         }
     }
 }
